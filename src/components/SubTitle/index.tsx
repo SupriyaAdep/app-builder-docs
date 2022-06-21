484ad0538a90e33c97c2b6f5dbf872fba621cf41
Bhupendra Negi <bhupendra1011@gmail.com>
2022-06-21 13:01:55 +0530

    fix for pathname

--- src/components/SubTitle/index.tsx
+++ src/components/SubTitle/index.tsx
@@ -4,32 +4,33 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function SubTitle({ children }) {
   const content = React.Children.toArray(children.props.children);
-  const slug = window.location.pathname;
 
   const heading = content.shift();
 
-  const data = content.map((element, index) => {
-    let link = "";
-    let text = "";
-    let target = "";
-    const isLast = content.length - 1 === index;
-    if (typeof element === "string") {
-      link = `${slug}#${element.trim().toLowerCase()}`;
-      text = element;
-    } else {
-      link = element?.props?.href || "";
-      text = element?.props?.children || "";
-      target = "_blank";
+  const data = content.map(
+    (element: string | React.ReactElement, index: number) => {
+      let link = "";
+      let text = "";
+      let target = "";
+      const isLast = content.length - 1 === index;
+      if (typeof element === "string") {
+        link = `#${element.trim().toLowerCase()}`;
+        text = element;
+      } else {
+        link = element?.props?.href || "";
+        text = element?.props?.children || "";
+        target = "_blank";
+      }
+      return (
+        <React.Fragment key={index}>
+          <a className={styles.link} href={link} target={target}>
+            {text}
+          </a>
+          {!isLast && <span className={styles.separator}>|</span>}
+        </React.Fragment>
+      );
     }
-    return (
-      <React.Fragment key={index}>
-        <a className={styles.link} href={link} target={target}>
-          {text}
-        </a>
-        {!isLast && <span className={styles.separator}>|</span>}
-      </React.Fragment>
-    );
-  });
+  );
 
   return (
     <div className={styles.container}>
