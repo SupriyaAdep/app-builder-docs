715a11345afa7571cd4ea5814cef28afe75442ee
Bhupendra Negi <bhupendra1011@gmail.com>
2022-07-27 22:55:29 +0530

    adding design changes

--- src/theme/Footer/Links/index.js
+++ src/theme/Footer/Links/index.js
@@ -7,7 +7,7 @@ export default function FooterLinks({ links, contact }) {
   return <div className='row'>
     <div className='col col--6 footer__contact'>
       <ul className='footer__items clean-list'>
-        <li className='footer__item'><FooterLogo logo={contact.textLogo} /></li>
+
         <li className='footer__item'> <FooterLogo logo={contact.logo} /></li>
         <li className='footer__item'> <a href={`tel:${contact.contactNumber}`} className='footer__link-item'>{contact.contactNumber}</a> </li>
         <li className='footer__item'><a href={`mailto:${contact.email}`} className='footer__link-item'> {contact.email} </a></li>
